01ebf4340488785945649fc929ac1a1c67652a02
Aszalea Calderon <aszalea_calderon@yahoo.com>
2021-01-15 20:49:42 -0800

    creating the indiviualized cards

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 
+
 const NavBar = () => {
+// constructor(){
+//   super()
+//   this.state={
+//     formInput:'',
+//   }
+// }
+
+
+
   return (
     <div className="blueBar">
       <h1 className="mainHeader"> Github Followers</h1>
       <div className="searchBar">
         <h3 className="searchHeader">Search</h3>
-      <input placeholder="search" label="search"></input>
+      <input name="search" placeholder="search" label="search"></input>
+      
+      <button>Search</button>
       </div>
     </div>
   )
 }
 
 export default NavBar
+//Create a formSubmit Handler in app so you can search
+//Try doing it with react-hook-forms
+//create the cards needed
